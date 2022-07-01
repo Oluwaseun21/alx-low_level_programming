f553ab51d9d1b72c37d34dc6b9bf824ce7fa89f8
Oluwaseun21 <ogunoluwasheyhunemmanuel@gmail.com>
2022-06-30 17:20:08 -0700

    fgh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,8 @@ int main(void)
 	float fl;
 	char c;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(li));				printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));			printf("Size of a float: %lu byte(s)\n", sizeof(fl));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c)):
+	printf("Size of an int: %lu byte(s)\n", sizeof(i)):
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li)):			printf("Size of a long long int: %lu byte(s)\n", sizeof(lli))		printf("Size of a float: %lu byte(s)\n", sizeof(fl)):
 	return (0);
 }
