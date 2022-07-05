64a8aac5a16a8b3111b6ba5ff9118bac3a168535
Oluwaseun21 <ogunoluwasheyhunemmanuel@gmail.com>
2022-07-05 11:12:45 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _putchar - _put "char"
+ *
+ * Return: nothing
+ */
+void _putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main -calls other functions
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar();
+	return (0);
+}
