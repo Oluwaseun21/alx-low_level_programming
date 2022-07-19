3f2cb79776e4d533b2d79946106243770b23a3ec
Oluwaseun21 <ogunoluwasheyhunemmanuel@gmail.com>
2022-07-18 18:09:50 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,19 +16,19 @@ unsigned int _strspn(char *s, char *accept)
 		unsigned int b = 0;
 			int i;
 
-			while (*s)
-		{
-					for (i = 0; accept[i]; i++)
-								{
-												if (*s == accept[i])
-																{
-																					b++;
-																									break;
-																												}
-															else if (accept[i + 1] == '\0')
-																				return (b);
-																	}
-							s++;
-								}
+	while (*s)
+{
+	for (i = 0; accept[i]; i++)
+{
+	if (*s == accept[i])
+{
+	b++;
+	break;
+}
+	else if (accept[i + 1] == '\0')
+	return (b);
+}
+	s++;
+}
 	return (b);
 }
