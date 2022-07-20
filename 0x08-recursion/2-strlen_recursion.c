8f09026aabd3363d3db1f87a9f1d6d9bc69d2636
Oluwaseun21 <ogunoluwasheyhunemmanuel@gmail.com>
2022-07-20 12:22:00 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Task: Writing a function that returns length of a string.
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+{
+	len++;
+	len += _strlen_recursion(s + 1);
+}
+	return (len);
+}
